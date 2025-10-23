a9f1710cae8cdadf93b5d316f2957f2308855367
Shukroh9 <olajumokeshukroh9@gmail.com>
2025-10-23 17:51:05 +0100

    30DaysOfJS

---
+++ Day1/day1excercises.js
@@ -0,0 +1,4 @@
+// write a single line comment says, comments can make code readable
+// comments can make code readable 
+// write another single comment which says, Welcome to 30DaysOfJavaScript
+// Welcome to 30DaysOfJavaScript
---
+++ helloworld.js
@@ -0,0 +1,9 @@
+
+let firstName = 'Asabeneh'
+let lastName = 'Yetayeh'
+let country = 'Finland'
+let city = 'Helsinki'
+let age = 100
+let isMarried = true
+
+console.log(firstName, lastName, country, city, age, isMarried)
